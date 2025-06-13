865a04d73705aa40750373409bf0adaaffa7685d
Loreto Notarantonio <loreto.n@gmail.com>
2025-06-13 09:13:14 +0200

    update D2025-06-13T09.13

--- Source/include/@a_decisionalVariables.h
+++ Source/include/@a_decisionalVariables.h
@@ -1,6 +1,6 @@
 //
 // updated by ...: Loreto Notarantonio
-// Date .........: 12-06-2025 10.31.15
+// Date .........: 13-06-2025 09.12.55
 //
 
 
@@ -9,13 +9,13 @@
 
     #define ln_ESP32_WROOM_32E_MODULE         1
     #define ln_ESP32_WROOM_32E_MODULE_2RELAY  2
-    #define ln_ESP32_BOARD_TYPE               ln_ESP32_WROOM_32E_MODULE
-    // #define ln_ESP32_BOARD_TYPE               ln_ESP32_WROOM_32E_MODULE_2RELAY
+    // #define ln_ESP32_BOARD_TYPE               ln_ESP32_WROOM_32E_MODULE
+    #define ln_ESP32_BOARD_TYPE               ln_ESP32_WROOM_32E_MODULE_2RELAY
 
     #define ln_DEVEL                          1
     #define ln_PRODUCTION                     2
-    #define ln_RELEASE_TYPE                   ln_DEVEL
-    // #define ln_RELEASE_TYPE                   ln_PRODUCTION
+    // #define ln_RELEASE_TYPE                   ln_DEVEL
+    #define ln_RELEASE_TYPE                   ln_PRODUCTION
 
 
 
